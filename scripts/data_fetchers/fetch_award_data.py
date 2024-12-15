ed50331afe34c7a26f19f54d6c6a22d8192c7630
Jasper Cooper <jaspercooper@gmail.com>
2024-12-15 13:33:29 -0800

    Create fetch_award_data.py

---
+++ scripts/data_fetchers/fetch_award_data.py
@@ -0,0 +1,9 @@
+"""
+Title: Fetch Award Data from USASpending.gov
+Description: This script queries the USAspending API to get data on awards to 8(a) firms
+Author: Jasper Cooper
+Usage:
+    - clarify
+Notes:
+    - notes here
+"""
