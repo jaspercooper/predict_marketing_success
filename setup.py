39c1a0a54bbb685ee387f3c30c0b231a0801a173
Jasper Cooper <jaspercooper@gmail.com>
2024-12-14 16:06:29 -0800

    basic directory setup

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="code",                # Package name
+    version="0.1.0",               # Version number
+    packages=find_packages(),      # Automatically find packages (like 'scripts')
+    install_requires=[],           # Add any dependencies if needed
+)
