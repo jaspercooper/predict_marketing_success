060a5c5c282685f52947c553ed996651ea239c0b
Jasper Cooper <jaspercooper@gmail.com>
2024-12-15 16:26:17 -0800

    Create substring utility function

--- scripts/utils.py
+++ scripts/utils.py
@@ -10,3 +10,7 @@ def comma_join(string_list):
     separator = ", "
     return separator.join(string_list)
 
+# Generic function for subsetting a list of strings
+def subset_string(string_list, substring):
+    subset = [item for item in string_list if substring in item]
+    return subset
